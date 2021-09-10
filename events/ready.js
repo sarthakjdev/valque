48845a7124c77bc210bc81fdeb6bfd2d8884dbcf
SarthakJain594 <jsarthak448@gmail.com>
2021-09-10 13:53:25 +0530

    Bot ready and working!!

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
     console.log(`==== Bot ready :: ${client.user.username} =====`)
-    const setStatus = () => client.user.setActivity(`Dev OP!`)
+    const setStatus = () => client.user.setActivity(`VALORANT!`)
     setStatus()
 }
